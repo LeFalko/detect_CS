b016d7afaedbd62aed78dda382bd1cf2d42cf159
Paul Vialkowitsch <paul.vialkowitsch@student.uni-tuebingen.de>
2021-06-24 14:21:51 +0200

    GUI
    added commentary and tried to rework plotting

--- gui.py
+++ gui.py
@@ -15,6 +15,7 @@ import sys
 import pandas as pd
 
 
+# Initializing GUi window and setting size
 class Frame(QMainWindow):
     def __init__(self):
         super().__init__()
@@ -30,17 +31,19 @@ class Frame(QMainWindow):
         self.setCentralWidget(self.table_widget)
 
 
-# Initialize arrays
-raw_signal = []
-hig_pass_signal = []
-x_axis = []
-
-
+# creating content for the Frame
 class Content(QWidget):
     def __init__(self, parent):
         super(QWidget, self).__init__(parent)
         self.layout = QGridLayout()
         "self.popup_window = QWidget()"
+
+        # Initialize arrays
+        self.RAW = []
+        self.HIGH = []
+        self.Labels = []
+        self.Interval_inspected = []
+        self.sampling_rate = 25000
 
         # Initialize tab screen
         self.tabs = QTabWidget()
@@ -50,11 +53,9 @@ class Content(QWidget):
         self.tab_postprocessing = QWidget()
         self.tabs.resize(300, 200)
 
-        # Canvas
-        self.canvas = MplCanvas(self, width=5, height=4, dpi=100)
-        self.xdata = len(raw_signal)
-        self.ydata = raw_signal
-        self.update_plot()
+        # Figure to plot on
+        self.figure = Figure()
+        self.canvas = FigureCanvas(self.figure)
 
         # Add tabs
         self.tabs.addTab(self.tab_preprocessing, "Pre-processing")
@@ -75,6 +76,7 @@ class Content(QWidget):
         self.create_information_box()
         self.create_training_set_box()
 
+        # layout for first tab
         self.tab_preprocessing.layout.addWidget(self.data_input_box, 1, 0)
         self.tab_preprocessing.layout.addWidget(self.information_box, 0, 1)
         self.tab_preprocessing.layout.addWidget(self.training_set_box, 1, 1)
@@ -92,6 +94,7 @@ class Content(QWidget):
         # Add Groupboxes to second tab
         self.create_select_cs_box()
 
+        #layout for second tab
         self.tab_train.layout.addWidget(self.select_cs_box, 0, 0)
 
         self.tab_train.setLayout(self.tab_train.layout)
@@ -110,6 +113,7 @@ class Content(QWidget):
         self.layout.addWidget(self.tabs)
         self.setLayout(self.layout)
 
+    # creating a box in the first tab containing sampling rate input and file upload
     def create_data_input_box(self):
         data_input_layout = QGridLayout()
         data_input_layout.setColumnStretch(0, 4)
@@ -132,6 +136,7 @@ class Content(QWidget):
 
         self.data_input_box.setLayout(data_input_layout)
 
+    # creating the box in the first tab containing user information
     def create_information_box(self):
         information_layout = QGridLayout()
         information_layout.setColumnStretch(1, 1)
@@ -149,6 +154,7 @@ class Content(QWidget):
 
         self.information_box.setLayout(information_layout)
 
+    # creating the box in the first tab containing the train button
     def create_training_set_box(self):
         create_set_layout = QGridLayout()
         create_set_layout.setColumnStretch(0, 2)
@@ -165,12 +171,15 @@ class Content(QWidget):
 
         self.training_set_box.setLayout(create_set_layout)
 
+    # creating sampling input dialog
     def getInteger(self):
         i, okPressed = QInputDialog.getInt(self, "Enter sampling rate", "Sampling rate in Hz:", 25000, 0,
                                            100000, 1000)
         if okPressed:
-            print(i)
+            self.sampling_rate = i
+            return self.sampling_rate
 
+    # creating file upload dialog
     def openFileNameDialog(self):
         options = QFileDialog.Options()
         options |= QFileDialog.DontUseNativeDialog
@@ -178,43 +187,53 @@ class Content(QWidget):
                                                   "All Files (*);;MATLAB Files (*.mat)", options=options)
         if fileName:
             mat = sp.loadmat(fileName)
-            print(mat)
-            raw_data = mat['RAW']
-            raw_signal = raw_data[0]
-            print(raw_signal)
-            '''i = 0
-            while i < len(raw_signal):
-                x_axis[i] = i
-                i = i + 1
-            print(x_axis)'''
+            self.RAW = np.array(mat['RAW'])
+            self.HIGH = np.array(mat['HIGH'])
+            self.Labels = np.array(mat['Labels'])
+            self.Interval_inspected = np.array(mat['Interval_inspected'])
+            print(self.RAW)
+            #print(self.HIGH)
+            #print(Labels)
+            #print(Interval_inspected)
+            #dict_keys(['RAW', 'HIGH', 'Labels', 'Interval_inspected'])
             self.update_plot()
-            # high_pass_data = mat['HIGH']
-            # print(high_pass_data[0])
-            # high_pass_signal = high_pass_data[0]
-            plt.plot(raw_signal)
-            plt.show()
-            plt.ion()
 
+            return self.RAW, self.HIGH, self.Labels, self.Interval_inspected
+
+
+    # creating canvas and toolbar for second tab
     def create_select_cs_box(self):
         select_cs_layout = QGridLayout()
         select_cs_layout.setColumnStretch(0, 1)
         select_cs_layout.setColumnStretch(1, 1)
 
-        self.canvas.axes.plot(raw_signal)
-
         toolbar = NavigationToolbar(self.canvas, self)
+
+        plot_button = QPushButton('Plot')
+        plot_button.clicked.connect(self.update_plot)
 
         select_cs_layout.addWidget(toolbar, 0, 0)
         select_cs_layout.addWidget(self.canvas, 1, 0)
+        select_cs_layout.addWidget(plot_button, 0, 1)
 
         self.select_cs_box.setLayout(select_cs_layout)
 
+    # updating plot for new data?
     def update_plot(self):
-        self.canvas.axes.cla()
-        self.canvas.axes.plot(raw_signal, 'r')
+        data = self.RAW
+
+        ax = self.figure.add_subplot(111)
+
+        ax.clear()
+
+        ax.plot(data, '*')
+
         self.canvas.draw()
+        #plt.plot(np.arange(sampling_rate) / sampling_rate, RAW[0:sampling_rate])
+        #plt.xlabel('time (s)')
+        #plt.show()
 
-
+# canvas initiation
 class MplCanvas(FigureCanvas):
     def __init__(self, parent=None, width=5, height=4, dpi=100):
         fig = Figure(figsize=(width, height), dpi=dpi)
